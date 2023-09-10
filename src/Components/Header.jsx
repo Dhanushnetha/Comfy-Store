648939dd37fea4b0541e000ab4a2ef20d19b3223
Dhanush Kumar Gujjari <2152751@cognizant.com>
2023-09-10 18:23:22 +0530

    Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux";
-import { clearCart } from "../features/cart/cartSlice";
+// import { clearCart } from "../Features/Cart/cartSlice";
+import { clearCart } from "../Features/Cart/CartSlice"
 import { logoutUser } from "../Features/User/userSlice";
 import { useQueryClient } from "@tanstack/react-query";
 
